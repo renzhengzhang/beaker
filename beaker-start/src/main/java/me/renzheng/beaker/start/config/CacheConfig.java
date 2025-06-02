322b9cec16a17a007b29ebc09a84eb3b76a62be2
Renzheng <git@renzheng.me>
2025-06-02 15:01:19 +0800

    feat(start): add cache configuration
    - Create CacheConfig class to enable caching in the application
    - Add @EnableCaching annotation to activate caching features

---
+++ beaker-start/src/main/java/me/renzheng/beaker/start/config/CacheConfig.java
@@ -0,0 +1,13 @@
+package me.renzheng.beaker.start.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+
+/**
+ * Cache Configuration
+ *
+ * @author <a href="mailto:renzheng.zh@gmail.com">Renzheng Zhang</a>
+ */
+@EnableCaching
+public class CacheConfig {
+
+}
