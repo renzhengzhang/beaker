cf95e8137a43f733dccc55b335a961b01a75ba80
Renzheng <git@renzheng.me>
2025-06-02 15:01:09 +0800

    feat(security): enable method-level security
    - Add @EnableMethodSecurity annotation to WebSecurityConfig
    - This change allows for method-level security annotations to be used throughout the application

--- beaker-start/src/main/java/me/renzheng/beaker/start/config/WebSecurityConfig.java
+++ beaker-start/src/main/java/me/renzheng/beaker/start/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -24,6 +25,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @since 2024/4/19
  */
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class WebSecurityConfig {
 
