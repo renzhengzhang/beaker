834c923b656e1975bcb43ece159619a3b1807f86
Renzheng <git@renzheng.me>
2025-06-21 16:27:55 +0800

    feat(security): remove default HTTP basic authentication configuration

--- beaker-start/src/main/java/me/renzheng/beaker/start/config/WebSecurityConfig.java
+++ beaker-start/src/main/java/me/renzheng/beaker/start/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -83,7 +82,6 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(PERMIT_ALL_URLS).permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/login")
                         .permitAll())
